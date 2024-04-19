5c445fc4477d281accdbacced2a857ccdb77568b
Valchar85 <valchar234@gmail.com>
2024-04-18 21:25:25 -0300

    add js

---
+++ test/test-auth.js
@@ -0,0 +1,11 @@
+document.getElementById("login").addEventListener("click", function() {
+    console.log("Login clicked")
+})
+
+document.getElementById("password").addEventListener("click", function() {
+    console.log("Password clicked")
+})
+
+document.getElementById("button").addEventListener("click", function() {
+    console.log("Button clicked")
+})
