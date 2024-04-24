2b669729b3ece617dd00eba1f2d1810fc764d7b1
Valchar85 <valchar234@gmail.com>
2024-04-23 21:02:16 -0300

    add

--- test/test-auth.js
+++ test/test-auth.js
@@ -9,3 +9,24 @@ document.getElementById("password").addEventListener("click", function() {
 document.getElementById("button").addEventListener("click", function() {
     console.log("Button clicked")
 })
+document.addEventListener('DOMContentLoaded', function() {
+    const loginList = [];
+    const passwordList = [];
+    const input1 = document.getElementById('login'); 
+    const input2 = document.getElementById('password');
+    const button = document.getElementById('button');
+    button.addEventListener('click', function() {
+        const login = input1.value;
+        if (login) {
+            loginList.push(login);
+            console.log('LoginList:', loginList);
+            input1.value = '';
+        }
+        const password = input2.value;
+        if (password) {
+            passwordList.push(password);
+            console.log('PasswordList:', passwordList);
+            input2.value = '';
+        }
+    })
+});
