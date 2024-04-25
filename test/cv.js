57403c4dd7d01627f7874914675fb845fe062c94
Valchar85 <valchar234@gmail.com>
2024-04-25 18:46:21 -0300

    add

---
+++ test/cv.js
@@ -0,0 +1,44 @@
+var point = document.getElementById("header");
+
+point.addEventListener("mouseenter",function() {
+    this.style.color = "#81d2ed";
+})
+point.addEventListener("mouseleave",function() {
+    this.style.color = "ghostwhite";
+})
+point.addEventListener("mouseenter",function() {
+    this.style.backgroundColor = "#8c8cb2b1";
+})
+point.addEventListener("mouseleave",function() {
+    this.style.backgroundColor = "hsl(195, 48%, 61%)";
+})
+
+var point = document.getElementById("left");
+
+point.addEventListener("mouseenter",function() {
+    this.style.color = "#81d2ed";
+})
+point.addEventListener("mouseleave",function() {
+    this.style.color = "ghostwhite";
+})
+point.addEventListener("mouseenter",function() {
+    this.style.backgroundColor = "#8c8cb2b1";
+})
+  point.addEventListener("mouseleave",function() {
+    this.style.backgroundColor = "hsl(195, 48%, 61%)";
+})
+
+var point = document.getElementById("right");
+
+point.addEventListener("mouseenter",function() {
+    this.style.color = "#81d2ed";
+})
+point.addEventListener("mouseleave",function() {
+    this.style.color = "ghostwhite";
+})
+ point.addEventListener("mouseenter",function() {
+    this.style.backgroundColor = "#8c8cb2b1";
+})
+point.addEventListener("mouseleave",function() {
+    this.style.backgroundColor = "hsl(195, 48%, 61%)";
+})
